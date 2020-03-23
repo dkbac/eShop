7b7f63182ff9cfce591f140b78867c2d7d59b4ff
bawcs <dkbac159@gmail.com>
2020-03-23 11:39:50 +0700

    Create Entities.

---
+++ eShop.Data/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public int SortOrder { get; set; }
+        public bool IsShowOnHome { get; set; }
+        public int? ParentID { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ eShop.Data/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string SEOAlias { get; set; }
+    }
+}
